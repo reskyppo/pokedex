3517c1f6d1abeb5568ebdb41b0d02fa08e686dda
Reskyppo <reskybeen@gmail.com>
2022-01-27 21:12:16 +0700

    create pokemon card component

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,71 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { useGetPokemonTypes } from "../hooks/useGetPokemonTypes";
+import { capitalizeFirstLetter, getTypeColor } from "../utils/function";
+
+type PokemonItem = {
+  url: string;
+  name: string;
+  image: string;
+};
+
+const PokemonCard = (props: PokemonItem) => {
+  const { name, image } = props;
+  const { data } = useGetPokemonTypes(name);
+  const type = data?.pokemon?.types[0]?.type?.name;
+
+  const Card = styled.div`
+    margin: 0.5rem;
+    text-align: center;
+    border-radius: 16px;
+    background-color: ${getTypeColor(type)};
+  `;
+  return (
+    <Card>
+      <Link to={`detail/${name}`}>
+        <img
+          css={css`
+            height: 7rem;
+            width: 7rem;
+            margin: auto;
+            display: block;
+          `}
+          src={image}
+          alt={name}
+        />
+        <h2
+          css={css`
+            margin: 0.5rem 0rem;
+          `}
+        >
+          {capitalizeFirstLetter(name)}
+        </h2>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            margin: 0.75rem 0rem;
+          `}
+        >
+          {data?.pokemon?.types?.map((type: any) => (
+            <div
+              css={css`
+                background-color: ${getTypeColor(type?.type?.name)};
+                margin: 0rem 0.25rem;
+                padding: 0.5rem 0.75rem;
+                border-radius: 8px;
+                border: 2px solid white;
+              `}
+            >
+              {capitalizeFirstLetter(type?.type?.name)}
+            </div>
+          ))}
+        </div>
+      </Link>
+    </Card>
+  );
+};
+
+export default PokemonCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,56 @@
-import { useGetPokemonsList } from "../hooks/useGetPokemonsList";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import PokemonCard from "../components/PokemonCard";
+import { useGetPokemonsList } from "../hooks/useGetPokemonsList";
+
+type PokemonItem = {
+  url: string;
+  name: string;
+  image: string;
+};
 
 const Home = () => {
-  const { data, loading, error } = useGetPokemonsList();
+  const [limit, setLimit] = useState<number>(20);
+
+  const { data } = useGetPokemonsList();
+  const sliceData = data?.pokemons?.results?.slice(0, limit);
   const Container = styled.div`
     background-color: #fff;
     max-width: 640px;
     margin: auto;
+    padding: 1rem 0rem;
   `;
   const Grid = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
   `;
-  const Card = styled.div`
-    margin: 0.5rem;
+  const LoadMore = styled.div`
+    width: 20rem;
+    border: 1px solid #f47340;
+    border-radius: 6px;
+    color: #f47340;
     text-align: center;
+    margin: 0.5rem auto;
+    cursor: pointer;
   `;
 
-  const Title = styled.h4`
-    text-decoration: none;
-  `;
   return (
     <Container>
       <Grid>
-        {data?.pokemons?.results?.map((result: any) => (
-          <Card>
-            <Link to={`detail/${result?.name}`}>
-              <img src={result?.image} alt={result?.name} />
-              <Title>{result?.name}</Title>
-              <p>owned: 0</p>
-            </Link>
-          </Card>
+        {sliceData?.map((result: PokemonItem) => (
+          <PokemonCard
+            name={result?.name}
+            image={result?.image}
+            url={result?.url}
+          />
         ))}
       </Grid>
+      {limit < 100 && (
+        <LoadMore onClick={() => setLimit(limit + 20)}>
+          <h4>Load More</h4>
+        </LoadMore>
+      )}
     </Container>
   );
 };
