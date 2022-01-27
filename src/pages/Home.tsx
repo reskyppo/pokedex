9a46848b4cd0d85f6d0c75eef5127a1901bf1ac3
Reskyppo <reskybeen@gmail.com>
2022-01-27 09:35:42 +0700

    init implement graphql

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import { useGetPokemonsList } from "../hooks/useGetPokemonsList";
 type Props = {};
 
 const Home = (props: Props) => {
+  const { data, loading, error } = useGetPokemonsList();
+  console.log("data", data.pokemons);
   return (
     <div>
       <h3>home page</h3>
+      {data?.pokemons?.results?.map((result: any) => (
+        <div>
+          <img src={result?.image} alt={result?.name} />
+          <p>{result?.name}</p>
+          <p>owned: 0</p>
+        </div>
+      ))}
     </div>
   );
 };
